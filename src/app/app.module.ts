ad0246109ba13bb6f55f9e59e609c4fc6b4ad548
bron.teixeira.s@gmail.com <bron.teixeira.s@gmail.com>
2021-06-02 10:50:28 -0300

    icones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { CabecalhoComponent } from './shared/cabecalho/cabecalho.component';
+import {MatIconModule} from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 
 
+import { CabecalhoComponent } from './shared/cabecalho/cabecalho.component';
 import { AppRoutingModule, routing } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
@@ -33,6 +34,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     HttpClientModule,
     routing,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
