69f80dcfd9c653cee15daddf26502b6f1f42f167
bron.teixeira.s@gmail.com <bron.teixeira.s@gmail.com>
2021-06-10 11:12:48 -0300

    inicio ngClass

--- src/app/pages/listagem/listagem.component.ts
+++ src/app/pages/listagem/listagem.component.ts
@@ -3,12 +3,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Subject, pipe } from 'rxjs';
-import {distinctUntilChanged, debounceTime,  switchMap,} from 'rxjs/operators';
+import {distinctUntilChanged, debounceTime,  switchMap, elementAt,} from 'rxjs/operators';
 import { of } from 'rxjs';
 
 import { CervejasService } from 'src/app/shared/models/cervejas.service';
 import { Cervejas } from '../../shared/models/Cervejas';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 
 
@@ -25,7 +26,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 export class ListagemComponent implements OnInit {
   queryField = new FormControl();
-  cervejas: Cervejas[] = []
+  cervejas: Cervejas[] = [];
+  troca:boolean = false;
 
   constructor(private service: CervejasService,
     private http: HttpClient,
@@ -45,11 +47,22 @@ pesquisaCerveja(){
   if(this.queryField.value === '' || this.queryField.value === null){
     this.abrirSnackbar()
   };
+
+  const found = this.cervejas.find((element:any) => this.cervejas = element);
+
+  console.log(found);
+
     this.service.pesquisaCerveja(this.queryField.value)
     .subscribe((dados)=> this.cervejas = dados );
+
+
   }
 
 abrirSnackbar(){
-  this._snackBar.open('Coloque uma cerveja', 'sair')
+  this._snackBar.open('Coloque uma cerveja', 'sair', { duration: 2000, panelClass: ['hotpink'] })
+}
+
+trocaCard(){
+  this.troca = !this.troca
 }
 }
